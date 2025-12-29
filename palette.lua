92b2fbe3643456296d326e35640e9a6130a6d352
zxrium <madvoidscripts@gmail.com>
2025-12-29 22:14:35 +0800

    Create palette.lua

---
+++ palette.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        Name = "Coinite",
+        Base = {Color = Color3.fromRGB(255, 190, 124), Material = Enum.Material.Metal},
+        Secondary = {Color = Color3.fromRGB(255, 229, 184), Material = Enum.Material.Metal},
+        Tertiary = {Color = Color3.fromRGB(255, 231, 207), Material = Enum.Material.Metal},
+        Glow = {Color = Color3.fromRGB(206, 172, 124), Transparency = 0.6},
+    },
+    {
+        Name = "Gargantuan",
+        Base = {Color = Color3.fromRGB(126, 81, 66), Material = Enum.Material.Metal},
+        Secondary = {Color = Color3.fromRGB(81, 51, 41)},           -- No material → color only
+        Tertiary = {Color = Color3.fromRGB(193, 138, 106), Material = Enum.Material.Neon},
+        Glow = {Color = Color3.fromRGB(255, 170, 85), Transparency = 0.3},
+    },
+    {
+        Name = "Heavenite",
+        Base = {Color = Color3.fromRGB(255, 200, 155), Material = Enum.Material.Foil},
+        Secondary = {Color = Color3.fromRGB(168, 119, 96)},         -- No material → color only
+        Tertiary = {Color = Color3.fromRGB(191, 167, 133), Material = Enum.Material.Neon},
+        Glow = {Color = Color3.fromRGB(255, 218, 165), Transparency = 0.4},
+    },
+    {
+        Name = "Ceyite",
+        Base = {Color = Color3.fromRGB(53, 53, 53), Material = Enum.Material.Marble},
+        Secondary = {Color = Color3.fromRGB(39, 39, 39)},           -- No material → color only
+        Tertiary = {Color = Color3.fromRGB(10, 10, 10), Material = Enum.Material.Fabric},
+        Glow = {Color = Color3.fromRGB(175, 175, 175), Transparency = 0.7},
+    },
+    {
+        Name = "Galaxite",
+        Base = {Color = Color3.fromRGB(0, 0, 0), Material = Enum.Material.Neon},
+        Secondary = {Color = Color3.fromRGB(23, 16, 43)},           -- No material → color only
+        Tertiary = {Color = Color3.fromRGB(23, 16, 43)},            -- No material → color only
+        Glow = {Color = Color3.fromRGB(141, 105, 186), Transparency = 0.2},
+    },
+    {
+        Name = "Starite",
+        Base = {Color = Color3.fromRGB(168, 132, 79), Material = Enum.Material.Foil},
+        Secondary = {Color = Color3.fromRGB(166, 130, 79)},         -- No material → color only
+        Tertiary = {Color = Color3.fromRGB(132, 123, 112)},         -- No material → color only
+        Glow = {Color = Color3.fromRGB(255, 236, 158), Transparency = 0.3},
+    },
+}
